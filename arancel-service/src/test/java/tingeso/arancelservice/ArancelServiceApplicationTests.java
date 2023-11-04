af547956f1bfae213005d756c1d96b83c4a1ae92
Orlando Solís <orlando.solis.d@usach.cl>
2023-11-03 21:08:02 -0300

    Creación de microservicio arancel

---
+++ arancel-service/src/test/java/tingeso/arancelservice/ArancelServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package tingeso.arancelservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArancelServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
